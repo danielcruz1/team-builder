983f9230357db0ae41182cce2056ec411444ecad
Daniel Cruz <d.cruz.489@hotmail.com>
2019-10-08 19:52:06 -0600

    set up state and key value pairs for team member info

--- team/src/App.js
+++ team/src/App.js
@@ -1,8 +1,16 @@
-import React from 'react';
+import React, {useState} from 'react';
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
+
+  const [teamMemeberInfo, setTeamMemberInfo] = useState({
+    name: '',
+    email:'',
+    role:'',
+  });
+
+
   return (
     <div className="App">
       <header className="App-header">
